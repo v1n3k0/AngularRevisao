9a09384cb03d5a1986c434ea354cdb76cb1a72b1
Vinicius <v1n1c1u5_1@hotmail.com>
2025-12-05 17:15:39 -0300

    aula 101

---
+++ app1/src/app/painel/frases-mock.ts
@@ -0,0 +1,9 @@
+import { Frase } from "../shared/frase.model"
+
+const FRASES: Frase[] = [
+    new Frase('I like to learn', 'Eu gosto de aprender'),
+    new Frase('I watch tv', 'Eu assisto tv'),
+    new Frase('How are you?', 'Como você está?'),
+    new Frase('I eat bread', 'Eu como pão'),
+    new Frase('I am Brazilian', 'Eu sou brasileiro')
+]
